7eb96711094b5deaff542664c680f8ec6f4f7abf
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-05-12 03:27:41 +0600

    set search functionality

--- src/pages/Queries/Queries.jsx
+++ src/pages/Queries/Queries.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import { Link, useLoaderData, useParams } from "react-router-dom";
 
 const Queries = () => {
-    const queries = useLoaderData()
-    const { id } = useParams()
-    console.log(id);
-    console.log(queries);
-    return (
-        <div className="grid grid-cols-4 my-10 gap-5 mx-40">
-                {/* <div className="col-span-1">
+  const queries = useLoaderData()
+  const { id } = useParams()
+  console.log(id);
+  console.log(queries, 'fgfg');
+
+  const [need, setNeed] = useState('')
+
+  const filteredProucts = queries.filter((product) => product.name.toLowerCase().includes('Denim'))
+  console.log(filteredProucts);
+  return (
+    <>
+      <form className="from-current mt-10 mx-40">
+      <label className="input input-bordered flex items-center gap-2">
+        <input onChange={(e) => setNeed(e.target.value.toLocaleLowerCase())}
+         type="text" className="grow" placeholder="Search" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+      </label>
+      </form>
+      <div className="grid grid-cols-4 my-10 gap-5 mx-40">
+        {/* <div className="col-span-1">
                 <Card></Card>
                 </div>
                 <div className="col-span-1">
@@ -19,18 +33,21 @@ const Queries = () => {
                 <div className="col-span-1">
                 <Card></Card>
                 </div> */}
-                {
-                    queries.map(p => <div key={p._id} className="card col-span-1 card-compact bg-base-100 shadow-xl p-x-5 ">
-                    <figure><img className="h-[300px]  object-cover " src={p.photo} alt="Shoes" /></figure>
-                    <div className="card ">
-                      <h1></h1>
-                      <Link to={`/queriDetels/${p._id}`}  className="btn bg-[#f16179] "> Recommend </Link>
-                    </div>
-                  </div>)
-                }
-
+        {
+          queries.filter((product) => product.name.toLowerCase().includes(need)).map(p => <div key={p._id} className="card col-span-1 card-compact bg-base-100 shadow-xl p-x-5 ">
+            <figure><img className="h-[300px]  object-cover " src={p.photo} alt="Shoes" /></figure>
+            <div className="card ">
+              <h1 className="font-bold text-2xl text-center">{p.name.slice(0, 14)}</h1>
+              <Link to={`/queriDetels/${p._id}`} className="btn bg-[#f16179] "> Recommend </Link>
             </div>
-    );
+          </div>)
+        }
+
+      </div>
+
+
+    </>
+  );
 };
 
 export default Queries;
